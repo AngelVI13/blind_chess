1363dd56bb20cb5a2e90ea8016aa28426c9a93bb
Angel VI <a.v.iliev13@gmail>
2022-11-10 15:17:00 +0200

    wip: square

--- pkg/game/square.go
+++ pkg/game/square.go
@@ -1,63 +1,112 @@
 package game
 
-var out4 string = `
-/** Square color enum */
-export enum Color {
-    White,
-    Black
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Color int
+
+const (
+	White Color = iota
+	Black
+)
+
+var (
+	Files = []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	Ranks = []int{1, 2, 3, 4, 5, 6, 7, 8}
+)
+
+const (
+	// 32-bit bitmask to determine the color of squares (all 1's represent black)
+	ColorBitboard uint32 = 0xAA55AA55
+
+	FileNum = 8
+	RankNum = 8
+)
+
+type Square struct {
+	file int
+	rank int
 }
 
+func NewSquare(file, rank int) (*Square, error) {
+	if file < 0 || file >= FileNum || rank < 0 || rank >= RankNum {
+		return nil, fmt.Errorf("Ivalid rank/file: f-%d r-%d", file, rank)
+	}
+	return &Square{
+		file: file,
+		rank: rank,
+	}, nil
+}
 
-const FILES: string[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
-// create an array of numbers from 1-8 (inclusive)
-const RANKS: number[] = Array.from(new Array(8), (x, i) => i + 1);
-// 32-bit bitmask to determine the color of squares (all 1's represent black)
-const COLOR_BITBOARD: number = 0xAA55AA55;
+// NewSquareFromIndex constructor for creating Square objects from 64-based index.
+func NewSquareFromIndex(index int) (*Square, error) {
+	if index < 0 || index >= (FileNum*RankNum) {
+		return nil, fmt.Errorf("Wrong index: %d. Index should be in range [0, 63]", index)
+	}
 
+	return &Square{
+		rank: index / 8,
+		file: index % 8,
+	}, nil
+}
 
-export class Square {
-    file: number;
-    rank: number;
+// NewSquareFromNotation constructor for creating Square objects
+// from square notation ("a1", "d3").
+func NewSquareFromNotation(notation string) (*Square, error) {
+	if len(notation) != 2 {
+		return nil, fmt.Errorf(
+			"Wrong square notation format %s, expected format \"b7\"",
+			notation,
+		)
+	}
 
-    constructor(file: number, rank: number) {
-        if (!(0 <= file && file < 8) || !(0 <= rank && rank < 8)) {
-            throw new Error('Ivalid rank/file: f-${file} r-${rank}');
-        }
+	notation = strings.ToLower(notation)
 
-        this.file = file;
-        this.rank = rank;
-    }
+	fileStr := string(notation[0])
+	rankStr := string(notation[1])
 
-    /** Alternative constructor for creating Square objects from 64-based index*/
-    static fromIndex(index: number): Square {
-        if (!(0 <= index && index < 64)) {
-            throw new Error('Wrong index: ${index}. Index should be in range [0, 63]');
-        }
-        let rank = Math.floor(index / 8);
-        let file = index % 8;
-        return new this(file, rank);
-    }
+	rankInt, err := strconv.Atoi(rankStr)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"Wrong square notation %s, rank is not a number",
+			notation,
+		)
+	}
 
-    /** Alternative constructor for creating Square objects from square notation (i.e. "a1", "d3"*/
-    static fromNotation(notation: string): Square {
-        if (notation.length !== 2) {
-            throw new Error('Wrong square format ${notation}');
-        }
+	fileIdx, fileFound := Contains(Files, fileStr)
+	rankIdx, rankFound := Contains(Ranks, rankInt)
 
-        notation = notation.toLowerCase();
+	if !fileFound || !rankFound {
+		return nil, fmt.Errorf(
+			"Wrong square notation %s, rank/file not found",
+			notation,
+		)
+	}
 
-        let fileStr = notation[0];
-        let rankStr = notation[1];
+	return &Square{
+		file: fileIdx,
+		rank: rankIdx,
+	}, nil
+}
 
-        if (!(FILES.includes(fileStr)) || !(RANKS.includes(parseInt(rankStr)))) {
-            throw new Error('Wrong square format ${notation}');
-        }
+func Contains[T comparable](s []T, e T) (int, bool) {
+	for i, v := range s {
+		if v == e {
+			return i, true
+		}
+	}
+	return -1, false
+}
 
-        let file = FILES.indexOf(fileStr);
-        let rank = RANKS.indexOf(parseInt(rankStr));
+// Index 64-based index of the square
+func (s *Square) Index() int {
+	return (s.rank * 8) + s.file
+}
 
-        return new this(file, rank);
-    }
+var out4 string = `
 
     /** Return a 64-based index of the square */
     index(): number {
